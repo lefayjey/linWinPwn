db307301611cf34f522c64cf58abefd21235e193
lefayjey <joey.farjallah@gmail.com>
2022-02-07 19:26:10 +0100

    Update update_dns.sh

--- update_dns.sh
+++ update_dns.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+# Run if you're having DNS issues while connecting to target Domain Controller
+#
+# WARNING:
+# This will update /etc/resolv.conf and could cause internet connectivity issues
+# Please backup /etc/resolv.conf before execution and then revert it in case of issues
 
-if [ -z "$1" ]
-  then
+if [ -z "$1" ]; then
     echo "IP of Domain Controller/DNS server not supplied"
     echo "Usage: $0 <IP>"
+    exit
 fi
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
   exit
 fi
 
